8d172d4a1ba06fe1403d12c8e2153ee8a64e3570
Pedromn88 <81178931+Pedromn88@users.noreply.github.com>
2021-04-05 21:11:58 +0200

    Acabada

--- 2_tema/Opcional/app.js
+++ 2_tema/Opcional/app.js
@@ -18,7 +18,7 @@ var hoteles = {
     "Indique hotel sobre el que quiere hacer la reseña: Mencey o Lani",
   );
 
-  document.getElementById("name-hotel").innerHTML =
+document.getElementById("name-hotel").innerHTML =
   "Hotel " + hoteles[selectedHotel].name;
 
 document.getElementById("location-hotel").innerHTML =
@@ -27,20 +27,18 @@ document.getElementById("location-hotel").innerHTML =
 document.getElementById("img-hotel").src = hoteles[selectedHotel].img;
 
 var stars = {
-    una:
-      "<span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
-    dos:
-      "<span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
-    tres:
-      "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span>",
-    cuatro:
-      "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9734;</span>",
-    cinco:
-      "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>",
-  };
+  una:
+    "<span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
+  dos:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
+  tres:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span>",
+  cuatro:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9734;</span>",
+  cinco:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>",
+};
 
-  var rating = prompt("Puntuación: una, dos, tres, cuatro o cinco estrellas");
+var rating = prompt("Puntuación: una, dos, tres, cuatro o cinco estrellas");
 
-  document.getElementById("rating").innerHTML = stars[rating];
-
-
+document.getElementById("rating").innerHTML = stars[rating];
