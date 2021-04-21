bf22da0ee809f74c799ff9c1fcbbf0993ee1c53b
Pedromn88 <81178931+Pedromn88@users.noreply.github.com>
2021-04-21 19:07:50 +0200

    laboratorio

---
+++ 3_tema_laboratorio/labs.js
@@ -0,0 +1,29 @@
+const product = { count: 3, price: 12.55, type: "ropa" };
+
+let total; 
+
+
+if (product.count > 0) {
+     total = product.count * product.price
+} else {
+    total = 0
+}
+
+console.log(total)
+
+let tipoIVA = 0.21
+
+switch (product.type) {
+
+    case "alimentacion": 
+        tipoIVA = 0.10;
+    break;
+
+    case "libro":
+        tipoIVA = 0.04;
+    break;
+}
+
+const IVAtotal = total * tipoIVA
+console.log (IVAtotal)
+console.log (total + IVAtotal)
