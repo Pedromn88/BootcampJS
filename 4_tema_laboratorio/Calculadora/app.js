68b66bea83f3243846dc512347f93550d3f87628
Pedromn88 <81178931+Pedromn88@users.noreply.github.com>
2021-05-20 21:18:23 +0200

    Ejercicio 6.1
    Ejercicio 6.1

--- 4_tema_laboratorio/Calculadora/app.js
+++ 4_tema_laboratorio/Calculadora/app.js
@@ -1,34 +1,34 @@
-//var number1 = () => parseInt(document.getElementById("numberA").value)
-//var number2 = () => parseInt(document.getElementById("numberB").value)
+var number1 = () => parseInt(document.getElementById("numberA").value)
+var number2 = () => parseInt(document.getElementById("numberB").value)
 
 
-//var sumNumber = () => number1() + number2()
-//var restNumber = () => number1() - number2()
-//ar multiNumber = () => number1() * number2()
-//var diviNumber = () => number1() / number2()
+var sumNumber = () => number1() + number2()
+var restNumber = () => number1() - number2()
+var multiNumber = () => number1() * number2()
+var diviNumber = () => number1() / number2()
 
-//var showsuma = () =>
-//document.getElementById("resultado").innerText = sumNumber();
-//var showresta = () =>
-//document.getElementById("resultado").innerText = restNumber();
-//var showmulti = () =>
-//document.getElementById("resultado").innerText = multiNumber ();
-//var showdivision = () =>
-//document.getElementById("resultado").innerText = diviNumber ();
+var showsuma = () =>
+document.getElementById("resultado").innerText = sumNumber();
+var showresta = () =>
+document.getElementById("resultado").innerText = restNumber();
+var showmulti = () =>
+document.getElementById("resultado").innerText = multiNumber ();
+var showdivision = () =>
+document.getElementById("resultado").innerText = diviNumber ();
 
 
-//document.getElementById("suma").addEventListener("click", showsuma);
+document.getElementById("suma").addEventListener("click", showsuma);
 
-//document.getElementById("resta").addEventListener("click", showresta);
+document.getElementById("resta").addEventListener("click", showresta);
 
-//document.getElementById("multi").addEventListener("click", showmulti);
+document.getElementById("multi").addEventListener("click", showmulti);
 
-//document.getElementById("division").addEventListener("click", showdivision);
+document.getElementById("division").addEventListener("click", showdivision);
 
 //CALCULADORA SECUENCIAL
 
 
-var numeroAnterior = document.getElementById("numero_anterior")
+/*var numeroAnterior = document.getElementById("numero_anterior")
 var numeroActual = document.getElementById("numero_actual").valor;
 
 var sumNumber = () => numeroAnterior() + numeroActual()
@@ -53,4 +53,4 @@ document.getElementById("resta").addEventListener("click", showresta);
 document.getElementById("multi").addEventListener("click", showmulti);
 
 document.getElementById("division").addEventListener("click", showdivision);
-
+*/
---
+++ 6.1_tema/app.js
@@ -0,0 +1,122 @@
+// Constantes.
+const REGULAR_TYPE = 21;
+const LOWER_TYPE = 4;
+const EXEMPT_TYPE = 0;
+
+// Entrada.
+const products = [
+  {
+    description: "Goma de borrar",
+    price: 0.25,
+    tax: LOWER_TYPE,
+    stock: 2,
+    units: 0,
+  },
+  {
+    description: "Lápiz H2",
+    price: 0.4,
+    tax: LOWER_TYPE,
+    stock: 5,
+    units: 0,
+  },
+  {
+    description: "Cinta rotular",
+    price: 9.3,
+    tax: REGULAR_TYPE,
+    stock: 2,
+    units: 0,
+  },
+  {
+    description: "Papelera plástico",
+    price: 2.75,
+    tax: REGULAR_TYPE,
+    stock: 5,
+    units: 0,
+  },
+  {
+    description: "Escuadra",
+    price: 8.4,
+    tax: REGULAR_TYPE,
+    stock: 3,
+    units: 0,
+  },
+  {
+    description: "Pizarra blanca",
+    price: 5.95,
+    tax: REGULAR_TYPE,
+    stock: 2,
+    units: 0,
+  },
+  {
+    description: "Afilador",
+    price: 1.2,
+    tax: LOWER_TYPE,
+    stock: 10,
+    units: 0,
+  },
+  {
+    description: "Libro ABC",
+    price: 19,
+    tax: EXEMPT_TYPE,
+    stock: 2,
+    units: 0,
+  },
+];
+
+
+
+var container = document.getElementById("product-list-container");
+
+var createProduct = product => {
+  var input = document.createElement("input");
+  input.setAttribute("type", "number");
+  input.addEventListener("change", event => product.units = event.target.valueAsNumber);
+  container.appendChild(input);
+}
+
+var showProducts = productList => {
+  for (var product of productList) {
+    var description = document.createElement("h5");
+    description.innerText = product.description;
+    container.appendChild(description);
+    createProduct(product);
+  }
+}
+
+showProducts(products);
+
+document.getElementById("btn").addEventListener("click", () => console.log(products));
+
+
+
+var subtotal = 0
+for (i=0; i < products.length; i++) {
+ subtotal += products[i].price * products [i].units
+}
+console.log(subtotal)
+
+var impuestos = 0
+for (i=0; i < products.length; i++) {
+  if (products.tax == LOWER_TYPE) {
+   impuestos += products.price * 0.04
+ } else if (products.tax == REGULAR_TYPE){
+  impuestos += products.price * 0.21}
+  else if(products.tax == EXEMPT_TYPE) 
+ {impuestos += products.price * 0
+ } 
+}
+ 
+
+console.log(impuestos)
+
+var total = 0
+for (i=0; i < products.length; i++) {
+ total += subtotal + impuestos;
+console.log(total)
+}
+
+
+
+
+
+
