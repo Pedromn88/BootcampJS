0aa04acd53976a4a93da4bc1e8b7d31beee33be1
Pedromn88 <81178931+Pedromn88@users.noreply.github.com>
2021-06-03 21:02:36 +0200

    upload

---
+++ 6.3.1_tema/app.js
@@ -0,0 +1,58 @@
+// Constantes
+var WORK_HOURS = [
+    "08:00 - 09:00",
+    "09:00 - 10:00",
+    "10:00 - 11:00",
+    "11:00 - 12:00",
+    "12:00 - 13:00",
+    "13:00 - 14:00",
+    "15:00 - 16:00",
+    "16:00 - 17:00"
+  ];
+  
+  // Datos
+  var myTeam = [
+    {
+      name: "María",
+      availability: new Array(8).fill(true)
+    },
+    {
+      name: "Pedro",
+      availability: new Array(8).fill(true)
+    },
+    {
+      name: "Esther",
+      availability: new Array(8).fill(true)
+    },
+    {
+      name: "Marcos",
+      availability: new Array(8).fill(true)
+    },
+  ];
+  
+
+
+
+for (var i = 0; i < myTeam.length; i++) {
+  horario (myTeam[i])
+}
+
+function horario (empleado){
+  console.log(empleado.name)
+  for (var i = 0; i < WORK_HOURS.length; i++) {
+  var horarioRandom = (a, b) => Math.round(Math.random()) ?  a : b;
+  console.log(WORK_HOURS[i]); 
+  console.log(horarioRandom("si", "no"))
+  }
+ }
+
+
+  
+
+
+
+
+
+
+
+
