ebbc606c003d5e5e7eeabe0828e5862b47826c11
Pedromn88 <81178931+Pedromn88@users.noreply.github.com>
2021-06-03 21:42:59 +0200

    Update app.js

--- 6.3.1_tema/app.js
+++ 6.3.1_tema/app.js
@@ -37,22 +37,23 @@ for (var i = 0; i < myTeam.length; i++) {
   horario (myTeam[i])
 }
 
+
 function horario (empleado){
   console.log(empleado.name)
   for (var i = 0; i < WORK_HOURS.length; i++) {
-  var horarioRandom = (a, b) => Math.round(Math.random()) ?  a : b;
+  var horarioRandom = (a, b) => Math.round(Math.random()) ? a : b;
   console.log(WORK_HOURS[i]); 
   console.log(horarioRandom("si", "no"))
   }
  }
 
+ function buscarElemento(horarioRandom, elemento) {
+  for(let i = 0; i < horarioRandom.length; i++) {
+      if (horarioRandom[i] === elemento) {
+          return i;
+      }
+  }
+  return -1;
+}
 
-  
-
-
-
-
-
-
-
-
+console.log(buscarElemento, si)
