4170ea597be11eb1e9baa27dca85f23268e99858
Pedromn88 <81178931+Pedromn88@users.noreply.github.com>
2021-06-08 22:21:34 +0200

    update

--- 6.3.2_tema/app.js
+++ 6.3.2_tema/app.js
@@ -43,26 +43,30 @@ for(i=0; i<monedas.length; i++)
 var importe = () => document.getElementById("input-number1").value;
 var entregado = () => document.getElementById("input-number2").value;
 
-var monedas=[200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.20, 0.10, 0.05, 0.02, 0.01];
+//var resta = () => entregado() - importe();
 
+/*function suma() {
+    var resta =  entregado () - importe ();
+    var cambio20 = resta / 20
+    if (cambio20 >= 1) return cambio20 + "" + "Billetes de 200";
+    }*/
+    
 
 
 var cambioBilletes = function() {
-    var resta = () => entregado() - importe();
-for(var i=0; i<monedas.length; i++)
-var cambio200 = 0
-cambio200 = resta / monedas[0]
-if (cambio200 >= 1) return cambio200 + "Billetes de 200";
-/*var cambio100 = resta / 100
-if (cambio100 >= 1) return cambio100 + "Billetes de 100";
+var resta =  entregado () - importe ();
+var cambio200 = resta / 200
+if (cambio200 >= 1) return cambio200 + " " + "Billetes de 200";
+var cambio100 = resta / 100
+if (cambio100 >= 1) return cambio100 + " " + "Billetes de 100";
 var cambio50 = resta/ 50
-if (cambio50 >= 1) return cambio50 + "Billetes de 50";
+if (cambio50 >= 1) return cambio50 + " " + "Billetes de 50";
 var cambio20 = resta / 20
-if (cambio20 >= 1) return cambio20 + "Billetes de 20";
+if (cambio20 >= 1) return cambio20 + " " + "Billetes de 20";
 var cambio10 = resta / 10
-if (cambio10 >= 1) return cambio10 + "Billetes de 10";
+if (cambio10 >= 1) return cambio10 + " " + "Billetes de 10";
 var cambio5 = resta / 5
-if (cambio5 >= 1) return cambio5 + "Billetes de 5";*/
+if (cambio5 >= 1) return cambio5 + " " + "Billetes de 5";
 }
 
 
