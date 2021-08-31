c6e525b0a812e1fc052550d2acf1a0233b5c4e82
Pedromn88 <81178931+Pedromn88@users.noreply.github.com>
2021-08-31 19:56:56 +0200

    upload
    upload

--- 12_tema_inmobiliaria/src/pages/upload-property/upload-property-mappers.js
+++ 12_tema_inmobiliaria/src/pages/upload-property/upload-property-mappers.js
@@ -1,22 +1,28 @@
+export const mapPropertyUpdateListApiToVm = propertyList =>
+Array.isArray(propertyList)
+? getPropertyUpdateList.map(property => mapPropertyUpdateApiToVm(property))
+: [];
 
 
-
-export const mapPropertyUploadApiToVm = (property) => ({
+export const mapPropertyUpdateApiToVm = (property) => ({
     title: property.title,
     notes: property.notes, 
     email: property.email,
     phone: property.phone,
     price: property.price,
-    saleTypesIds:property.saletypes,
+    saletypes:property.saletypes,
     address: property.address,
     city: property.city,
-    provinceId: property.province,
+    province: property.province,
     squareMeter: `${property.squareMeter} m2`,
     rooms: property.rooms,
     bathrooms:property.bathrooms,
     locationUrl: property.locationUrl,
     mainFeatures: property.mainFeatures,
-    equipmentIds: property.equipmentIds,
-    images:Array.isArray(property.images) ? property.images[0] : '',
+    equipments: property.equipmentIds,
+    images:Array.isArray(property.images) ? property.images : [], 
 });
     
+
+
+
